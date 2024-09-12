65c8eed3203763a9b0ba5d2c7acb1fdd6ce42cc9
Aarjav Jain <anshjain638@gmail.com>
2024-09-12 14:10:53 +0530

    Setup the Project Structure

---
+++ Server/app.js
@@ -0,0 +1,38 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import colors from "colors";
+
+const app = express();
+const server = createServer(app);
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+  })
+);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello From Team Server");
+  console.log("Hello From Team Server".green.bgWhite);
+});
+
+const port = 3000;
+
+io.on("connection", (socket) => {
+  console.log("a user connected".green.bgWhite);
+  console.log(socket.id.bgBlue.white);
+});
+
+server.listen(port, () => {
+  console.log(`listening on ${port}`.green.bgYellow.bold);
+});
