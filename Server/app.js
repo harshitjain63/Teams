efe3dca5dc1dc37d03d59dca94ca8d6b2cfb94f3
Aarjav Jain <anshjain638@gmail.com>
2024-09-17 15:36:08 +0530

    modified the server file for better handling of events

--- Server/app.js
+++ Server/app.js
@@ -27,75 +27,77 @@ app.get("/", (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-const users = new Map();
+// const users = new Map();
+
+let users = [];
+
+const addUser = (userId, username, room) => {
+  const user = { userId, username, room };
+  users.push(user);
+  return user;
+};
+const removeUser = (userId) => {
+  users = users.filter((user) => user.userId !== userId);
+  return users;
+};
+const getUsersInRoom = (room) => {
+  return users.filter((user) => user.room === room);
+};
+
 io.on("connection", (socket) => {
+  let userId = socket.id;
   console.log("a user connected".green.bgWhite);
-  console.log(socket.id.bgBlue.white);
+  console.log(userId.bgBlue.white);
 
-  socket.emit("welcome", `Welcome ${socket.id}`);
+  socket.emit("welcome", `Welcome ${userId}`);
 
-  socket.on("join-room", (room, username) => {
-    socket.join(room);
-    users.set(socket.id, username);
+  socket.on("join-room", ({ room, username }) => {
     console.log(`${username} joined room ${room}`.green);
-    const roomSockets = io.sockets.adapter.rooms.get(room) || new Set();
-
-    const roomUsers = Array.from(roomSockets)
-      .map((socketId) => users.get(socketId))
-      .filter(Boolean);
-
-    io.to(socket.id).emit("current-users", roomUsers);
+    socket.join(room);
+    addUser(socket.id, username, room);
 
     io.to(room).emit("user-joined", `${username} has joined the room`);
+
+    io.to(room).emit("current-users", getUsersInRoom(room));
   });
 
-  socket.on("leave-room", (room, username) => {
+  socket.on("leave-room", ({ room, username }) => {
     socket.leave(room);
     console.log(`${username} left room ${room}`.red);
-    users.delete(socket.id);
+    removeUser(userId);
 
-    // Notify everyone in the room that the user has left
     io.to(room).emit("user-left", `${username} has left the room`);
 
-    // Update the list of current users in the room
-    const roomSockets = io.sockets.adapter.rooms.get(room) || new Set();
-    const roomUsers = Array.from(roomSockets)
-      .map((socketId) => users.get(socketId))
-      .filter(Boolean);
-
-    io.to(room).emit("current-users", roomUsers);
+    io.to(room).emit("current-users", getUsersInRoom(room));
   });
 
   // Handling user messages
-  socket.on("message", (msg) => {
+  socket.on("message", ({ room, username, text }) => {
     // Check if the user is in the room
-    const roomSockets = io.sockets.adapter.rooms.get(msg.room) || new Set();
-    const isInRoom = roomSockets.has(socket.id);
+    const isInRoom = users.some(
+      (user) => user.userId === userId && user.room === room
+    );
 
     if (isInRoom) {
-      console.log(`Message from ${msg.username}: ${msg.text}`.bgMagenta.white);
+      console.log(`Message from ${username}: ${text}`.bgMagenta.white);
       // Broadcast the message to everyone in the room
-      io.to(msg.room).emit("message", msg);
+      io.to(room).emit("message", { username, text });
     } else {
       console.log(
-        `User ${msg.username} attempted to send a message to a room they are not in.`
+        `User ${username} attempted to send a message to a room they are not in.`
       );
     }
   });
 
-  // Handle disconnect events
-  socket.on("disconnecting", () => {
-    console.log("disconnecting".red.bgWhite);
-    console.log(socket.id.bgBlue.white);
-  });
-
   socket.on("disconnect", () => {
-    const username = users.get(socket.id);
+    const user = users.find((user) => user.userId === userId);
 
-    if (username) {
-      users.delete(socket.id);
+    if (user) {
+      removeUser(userId);
 
-      console.log(`${username} disconnected`.red.bgWhite);
+      console.log(`${user.username} disconnected`.red.bgWhite);
+      io.to(user.room).emit("user-left", `${user.username} has left the room`);
+      io.to(user.room).emit("current-users", getUsersInRoom(user.room));
     }
   });
 });
